62d8c94484539d5d99cade042f8a9254a7701b7f
jrdhansen <jrdhansen@gmail.com>
2023-07-01 15:11:13 -0600

    Update test_regression_model files to successfully test trained forests model on test dataset

--- main.py
+++ main.py
@@ -90,18 +90,11 @@ def go(config: DictConfig):
             )
 
         if "train_random_forest" in active_steps:
-
-            # NOTE: we need to serialize the random forest configuration into JSON
+            # NOTE: serialize random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
-
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # YOUR CODE HERE #
-            ##################
+                json.dump(dict(config["modeling"]["random_forest"].items()), fp)
+            # Passing dict of forests hyperparameters via rf_config dict
             _ = mlflow.run(
                  os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
                  "main",
@@ -117,12 +110,14 @@ def go(config: DictConfig):
              )
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                parameters={
+                     "mlflow_model": "train_val_random_forest_model:prod",
+                     "test_dataset": "test_data.csv:latest"
+                 },
+             )
 
 
 if __name__ == "__main__":
